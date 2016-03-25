83e601cd88738ab703b2b4bc2191c82c53407050
pantol <pantolik@rambler.ru>
2016-03-25 22:27:16 +0300

    Добавил хождение ячейки стрелками

--- script3.js
+++ script3.js
@@ -28,6 +28,59 @@ function highlight(node)
     var cl=lit[c];
     document.getElementById('log').innerHTML+=' '+cl+r+', ';
 }
-window.onkeypress=function(event){
-    alert(event.keyCode);
+window.onkeydown=function(event){
+    switch(event.keyCode){
+        case 37:
+            var r = selectedTd.parentElement.rowIndex;
+            var c = selectedTd.cellIndex;
+            var cnew=c-1;
+            if(c<=1){cnew=1}
+            selectedTd.classList.remove('highlight');
+            selectedTd=myTable.rows[r].cells[cnew];
+            selectedTd.classList.add('highlight');
+            var cl=lit[cnew];
+            if(c>1){
+            document.getElementById('log').innerHTML+=cl+r+', ';                
+            }
+            break;
+        case 38:
+            var r = selectedTd.parentElement.rowIndex;
+            var c = selectedTd.cellIndex;
+            var rnew=r-1;
+            if(r<=1){rnew=1}
+            selectedTd.classList.remove('highlight');
+            selectedTd=myTable.rows[rnew].cells[c];
+            selectedTd.classList.add('highlight');
+            var cl=lit[c];
+            if(r>1){
+            document.getElementById('log').innerHTML+=cl+rnew+', ';                
+            }
+            break;
+        case 39:
+            var r = selectedTd.parentElement.rowIndex;
+            var c = selectedTd.cellIndex;
+            var cnew=c+1;
+            if(c>=8){cnew=8}
+            selectedTd.classList.remove('highlight');
+            selectedTd=myTable.rows[r].cells[cnew];
+            selectedTd.classList.add('highlight');
+            var cl=lit[cnew];
+            if(c<8){
+            document.getElementById('log').innerHTML+=cl+r+', ';                
+            }
+            break;
+        case 40:
+            var r = selectedTd.parentElement.rowIndex;
+            var c = selectedTd.cellIndex;
+            var rnew=r+1;
+            if(r>=8){rnew=8}
+            selectedTd.classList.remove('highlight');
+            selectedTd=myTable.rows[rnew].cells[c];
+            selectedTd.classList.add('highlight');
+            var cl=lit[c];
+            if(r<8){
+            document.getElementById('log').innerHTML+=cl+rnew+', ';                
+            }
+            break;            
+    }
 }
